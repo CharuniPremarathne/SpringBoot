8cf5a03633d214a567440d60a07cc687c24306ad
CharuniPremarathne <kpremarathne996@gmail.com>
2024-02-06 15:50:02 +0530

    add loggers

--- src/main/java/com/demo/controller/TaskController.java
+++ src/main/java/com/demo/controller/TaskController.java
@@ -2,6 +2,8 @@ package com.demo.controller;
 
 import com.demo.entity.Task;
 import com.demo.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,31 +14,57 @@ import java.util.Optional;
 @RequestMapping("/tasks")
 public class TaskController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+
     @Autowired
     private TaskService taskService;
 
     @GetMapping
-    public List<Task> findAllTasks(){
-        return taskService.findAllTasks();
+    public List<Task> findAllTasks() {
+        try {
+            return taskService.findAllTasks();
+        }catch(Exception e){
+            logger.error("Error in printing task details");
+            throw e;
+        }
     }
 
     @GetMapping("/{id}")
-    public Optional<Task> findTaskById(@PathVariable("id") int id){
-        return taskService.findById(id);
+    public Optional<Task> findTaskById(@PathVariable("id") int id) {
+        try {
+            return taskService.findById(id);
+        } catch (Exception e) {
+            logger.error("Can not print task details");
+            throw e;
+        }
     }
 
     @PostMapping("/saveTask")
-    public Task saveTask(@RequestBody Task task){
-        return taskService.saveTask(task);
+    public Task saveTask(@RequestBody Task task) {
+        try {
+            return taskService.saveTask(task);
+        } catch (Exception e) {
+            logger.error("Please check the values");
+            throw e;
+        }
     }
 
     @PutMapping("/updateTask")
-    public Task updateTask(@RequestBody Task task){
-        return taskService.updateTask(task);
+    public Task updateTask(@RequestBody Task task) {
+        try {
+            return taskService.updateTask(task);
+        }catch(Exception e){
+            logger.error("Update not succeed");
+            throw e;
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTask(@PathVariable("id") int id){
-        taskService.deleteTask(id);
+    public void deleteTask(@PathVariable("id") int id) {
+        try {
+            taskService.deleteTask(id);
+        }catch(Exception e){
+            logger.error("Delete not succeed");
+        }
     }
 }
--- src/main/java/com/demo/service/TaskService.java
+++ src/main/java/com/demo/service/TaskService.java
@@ -2,6 +2,8 @@ package com.demo.service;
 
 import com.demo.Repository.TaskRepo;
 import com.demo.entity.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,26 +13,53 @@ import java.util.Optional;
 @Service
 public class TaskService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskService.class);
+
     @Autowired
     private TaskRepo taskRepo;
 
-    public List<Task> findAllTasks(){
-        return taskRepo.findAll();
+    public List<Task> findAllTasks() {
+        try {
+            return taskRepo.findAll();
+        } catch (Exception e) {
+            logger.error("Error in finding all tasks");
+            throw e;
+        }
     }
 
-    public Optional<Task> findById(int id){
-        return taskRepo.findById(id);
+    public Optional<Task> findById(int id) {
+        try {
+            return taskRepo.findById(id);
+        }catch(Exception e){
+            logger.error("Error in finding the task");
+            throw e;
+        }
     }
 
-    public Task saveTask(Task task){
-        return taskRepo.save(task);
+    public Task saveTask(Task task) {
+        try {
+            return taskRepo.save(task);
+        }catch(Exception e){
+            logger.error("Error task saving");
+            throw e;
+        }
     }
 
-    public Task updateTask(Task task){
-        return taskRepo.save(task);
+    public Task updateTask(Task task) {
+        try {
+            return taskRepo.save(task);
+        }catch(Exception e){
+            logger.error("Error in task updating");
+            throw e;
+        }
     }
 
-    public void deleteTask(int id){
-        taskRepo.deleteById(id);
+    public void deleteTask(int id) {
+        try {
+            taskRepo.deleteById(id);
+        }catch(Exception e){
+            logger.error("Error in task deletion");
+            throw e;
+        }
     }
 }
